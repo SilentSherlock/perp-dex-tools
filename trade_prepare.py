a6ccc44f537b27022efd1689cfa4bc284e45eee6
SilentSherlock <1536141515@qq.com>
2025-12-07 22:23:13 +0800

    1.增加socket配置的代码

--- trade_prepare.py
+++ trade_prepare.py
@@ -8,3 +8,9 @@ def proxy_on():
     # 在代码中设置全局代理
     os.environ['HTTP_PROXY'] = 'http://127.0.0.1:10809'
     os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:10809'
+
+    # 配置socket代理
+    os.environ['ALL_PROXY'] = 'socks5://127.0.0.1:10808'
+    os.environ['all_proxy'] = 'socks5://127.0.0.1:10808'
+
+    print("Proxy enabled: HTTP/HTTPS on 10809, SOCKS5 on 10808")
